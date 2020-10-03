9bfc7c391d0c11e90fbb8bfe8c669586132dc94e
Ayush-Shyam-Kumar <65535504+Ayush-Shyam-Kumar@users.noreply.github.com>
2020-10-03 18:01:48 +0530

    Kmer Composition and Sorting Java Code

---
+++ Kmer Composition and Sorting/kmers.java
@@ -0,0 +1,66 @@
+package PRESENTATION2and3;
+
+import java.util.*;
+
+public class kmers {
+	public List<String> kmer_composition(String sequence, int kmerlen) // Method to obtain Kmer Composition
+	{
+		int windowstart = 0;
+		int windowend = kmerlen;
+		int looptime = sequence.length() - kmerlen + 1; // Total possible kmer to iterate the window over a loop
+		List<String> kmerlist = new ArrayList<String>(); // List to store all possible kmers
+		for (int i = 0; i < looptime; i++) {
+			// System.out.println(sequence.substring(windowstart,windowend));
+			kmerlist.add(sequence.substring(windowstart, windowend)); // appending all kmers to the list
+			windowstart++;
+			windowend++;
+		}
+		return kmerlist;
+	}
+
+	public List<String> lexographic_order(List<String> before_sort, int kmerlen) {
+		Object[] arraylist = before_sort.toArray();
+		int length = arraylist.length;
+		String[] stringArray = new String[length];
+		for (int i = 0; i < length; i++) {
+			stringArray[i] = (String) arraylist[i];
+		}
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = i + 1; j < stringArray.length; j++) {
+				if (stringArray[i].compareTo(stringArray[j]) > 0) {
+					String temp = stringArray[i];
+
+					stringArray[i] = stringArray[j];
+					stringArray[j] = temp;
+				}
+			}
+		}
+		return before_sort;
+	}
+
+	public void printlist(List<String> kmerlist) // A method to print all kmers obtained
+	{
+		for (String kmer : kmerlist)
+			System.out.print(kmer + " ");
+	}
+
+	// Test Funciton
+	public static void main(String[] args) {
+		kmers kmercomp = new kmers();
+		String sequence = new String();
+		sequence = "GCGGTAATGCAGTTGAC"; // Input Sequence
+		int kmerlen = 3; // Output Kmer Length (3mers)
+		System.out.print("Input Sequence : " + sequence);
+		int totalkmers = sequence.length() - kmerlen + 1;
+		// printing total possible kmers
+		System.out.print("\nTotal possible Kmers of length " + kmerlen + " in the sequence =  " + totalkmers);
+		System.out.print("\nKmers List : ");
+
+		List<String> kmerlist = kmercomp.kmer_composition(sequence, kmerlen); // storing the returned kmers list
+		kmercomp.printlist(kmerlist); // printing all kmers
+		// sorting the obtained kmers list in lexicographic order
+		List<String> sortedkmerslist = kmercomp.lexographic_order(kmerlist, kmerlen); 
+		System.out.print("\nAfter The lexographic order: ");
+		kmercomp.printlist(sortedkmerslist);
+	}
+}
